3729ef3dadc6a05b24fd308f340d1d20e842bc4a
Daniel Guldberg Aaes <dev@guldberglab.info>
2024-05-28 22:32:22 +0200

    [WIP], I know there is secrets here, I will handle that later on

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,23 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  define: {
+    "process.env": process.env,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
   },
 });
