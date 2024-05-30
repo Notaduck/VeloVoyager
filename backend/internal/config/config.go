f65d23d9bef207e64def4fed05cf27573bccffc2
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-05-30 19:59:29 +0200

    let's try to deploy

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	ServerPort         string `mapstructure:"SERVER_PORT"`
 	DbConnectionString string `mapstructure:"DB_CONNECTION_STRING"`
 	SupabaseUrl        string `mapstructure:"SUPABASE_URL"`
-	SupabaseKey        string `mapstructure:"SUPABASE_KEY"`
+	SupabaseKey        string `mapstructure:"SUPABASE_API_KEY"`
 	SupabaseJwtSecret  string `mapstructure:"SUPABASE_JWT_SECRET"`
 }
 
@@ -21,12 +21,12 @@ func NewConfig() *Config {
 	// Determine the environment
 	ENV := os.Getenv("ENV")
 
-	if ENV == "production" {
+	if ENV == "production" || ENV == "preview" {
 		// In production, read directly from environment variables
 		config.ServerPort = os.Getenv("SERVER_PORT")
 		config.DbConnectionString = os.Getenv("DB_CONNECTION_STRING")
 		config.SupabaseUrl = os.Getenv("SUPABASE_URL")
-		config.SupabaseKey = os.Getenv("SUPABASE_KEY")
+		config.SupabaseKey = os.Getenv("SUPABASE_API_KEY")
 		config.SupabaseJwtSecret = os.Getenv("SUPABASE_JWT_SECRET")
 	} else {
 		// In development, read from the .env file
