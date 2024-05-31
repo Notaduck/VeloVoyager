434631d35d5cfa1715ba2f1973b102139e47eca0
Daniel Guldberg Aaes <dev@guldberglab.info>
2024-05-31 20:11:24 +0200

    build and push backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,24 +6,26 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
-    define: {
-        "process.env": process.env,
+  plugins: [react(), TanStackRouterVite()],
+  define: {
+    "process.env": process.env,
+    VITE_SUPABASE_URL: process.env.SUPABASE_URL,
+    VITE_SUPABASE_API_KEY: process.env.SUPABASE_API_KEY,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-    base: "/",
-    preview: {
-        port: 8080,
-        strictPort: true,
-    },
-    server: {
-        port: 8080,
-        strictPort: true,
-        host: true,
-        origin: "http://0.0.0.0:8080",
-    },
+  },
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
