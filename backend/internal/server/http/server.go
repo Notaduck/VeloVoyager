0259ce08700865c9b0162ef117be80893e379cb8
Daniel Guldberg Aaes <dev@guldberglab.info>
2024-06-02 17:54:19 +0200

    build and push backend

--- backend/internal/server/http/server.go
+++ backend/internal/server/http/server.go
@@ -187,6 +187,7 @@ var originAllowlist = []string{
 	"http://127.0.0.1:3000",
 	"http://localhost:5173",
 	"http://localhost:3000",
+	"http://localhost:8080",
 	"http://frontend.localhost",
 	"http://frontend.localhost/",
 	"http://frontend.localhost:80/",
