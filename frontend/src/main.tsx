1845f557df5a0a0336823d4be791d287ff71303b
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-06-13 12:34:32 +0200

    sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,9 @@ import { routeTree } from "./routeTree.gen";
 import { createClient } from "@supabase/supabase-js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import * as Sentry from "@sentry/react";
+
+
 import "./globals.css";
 import "./index.css";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -35,6 +38,22 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
+
+
+Sentry.init({
+  dsn: "https://b98bdb716ccf15404e021d339d0a229a@o4507424145080320.ingest.de.sentry.io/4507424148881488",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 // Render the app
 const rootElement = document.getElementById("root")!;
